4a66c6ff0ec62c42c174d6971d89e67a98aed787
Mateusz <thatcinnamonrolldev@proton.me>
2026-06-19 18:42:29 +0200

    very simple list in playlist and tracklists blocks

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use ratatui::widgets::ListState;
 
 pub mod youtube;
 pub mod player;
@@ -6,9 +7,11 @@ pub mod playlists;
 pub mod tui;
 
 fn main() {
+    let mut list_state1 = ListState::default().with_selected(Some(0));
     let mut app = tui::app{
         playlists: None,
         tracklist: None,
+        list_state: list_state1,
         focused: tui::focused_block::Music,
         state: tui::menu_state::Playlist,
     };
--- src/tui.rs
+++ src/tui.rs
@@ -1,7 +1,7 @@
 use crossterm::event::{self, KeyCode};
 use ratatui::layout::{Constraint, Layout, Rect};
-use ratatui::style::Style;
-use ratatui::widgets::{Block, BorderType};
+use ratatui::style::{Style, Modifier};
+use ratatui::widgets::{Block, BorderType, List, ListState};
 use ratatui::Frame;
 use std::io::Result;
 
@@ -18,13 +18,14 @@ pub enum menu_state {
 pub struct app {
     pub playlists: Option<Vec<String>>,
     pub tracklist: Option<Vec<crate::playlists::song>>,
+    pub list_state: ListState,
     pub focused: focused_block,
     pub state: menu_state,
 }
 
 pub fn start_tui(app: &mut app) -> Result<()>{
     ratatui::run(|terminal| loop {
-        terminal.draw(|frame| app::render(&app,frame));
+        terminal.draw(|frame| app::render(app,frame));
 
         if let Some(key) = event::read()?.as_key_press_event() {
             match key.code {
@@ -42,6 +43,8 @@ pub fn start_tui(app: &mut app) -> Result<()>{
                         _ => {}
                     }
                 }
+                KeyCode::Char('w') | KeyCode::Up => {app.list_state.select_previous();}
+                KeyCode::Char('s') | KeyCode::Down => {app.list_state.select_next();}
                 _ => {}
 
             }
@@ -51,7 +54,7 @@ pub fn start_tui(app: &mut app) -> Result<()>{
 }
 
 impl app{
-    fn render(&self,frame: &mut Frame) {
+    fn render(&mut self,frame: &mut Frame) {
         let vertical = Layout::vertical([Constraint::Percentage(80), Constraint::Percentage(20)]);
         let [top, bottom] = frame.area().layout(&vertical);
 
@@ -64,27 +67,39 @@ impl app{
         }
 
         match self.state {
-            menu_state::Playlist => {Self::render_playlist_block(frame,top,top_block_style);}
-            menu_state::Tracklist => {Self::render_track_list_block(frame,top,top_block_style);}
+            menu_state::Playlist => {Self::render_playlist_block(frame,top,top_block_style,&mut self.list_state);}
+            menu_state::Tracklist => {Self::render_track_list_block(frame,top,top_block_style, &mut self.list_state);}
             _ => {}
         }
         Self::render_music_progress_bar_block(frame, bottom,music_block_style);
     }
 
-    fn render_track_list_block(frame: &mut Frame, area: Rect,block_style: Style) {
+    fn render_track_list_block(frame: &mut Frame, area: Rect,block_style: Style, list_state: &mut ListState) {
         let block = Block::bordered()
         .border_type(BorderType::Rounded)
         .border_style(block_style)
         .title("TrackList");
-        frame.render_widget(block, area);
+
+        let tracklist = ["song1","song2"];
+        let list = List::new(tracklist)
+        .highlight_style(Modifier::REVERSED)
+        .block(block);
+
+        frame.render_stateful_widget(list, area, list_state);
     }
 
-    fn render_playlist_block(frame: &mut Frame, area: Rect,block_style: Style) {
+    fn render_playlist_block(frame: &mut Frame, area: Rect,block_style: Style, list_state: &mut ListState) {
         let block = Block::bordered()
         .border_type(BorderType::Rounded)
         .border_style(block_style)
         .title("Playlists");
-        frame.render_widget(block, area);
+
+        let playlists = ["playlist1","playlist2"];
+        let list = List::new(playlists)
+        .highlight_style(Modifier::REVERSED)
+        .block(block);
+
+        frame.render_stateful_widget(list, area, list_state);
     }
 
     fn render_music_progress_bar_block(frame: &mut Frame, area: Rect,block_style: Style) {
