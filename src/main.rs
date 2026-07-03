e84778e53c94823e7e540ba243de18667c51c9ee
Mateusz <thatcinnamonrolldev@proton.me>
2026-07-03 19:41:22 +0200

    added simple file listing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::env;
+use gstreamer::glib::home_dir;
 use ratatui::widgets::ListState;
+
+use crate::playlists::playlist_helper;
 
 pub mod youtube;
 pub mod player;
@@ -8,15 +11,12 @@ pub mod tui;
 pub mod tui_helper;
 
 fn main() {
-    let mut list_state1 = ListState::default().with_selected(Some(0));
-    let mut list_state2 = ListState::default().with_selected(Some(0));
-    let mut app = tui::app{
-        playlists: None,
-        playlist_state: list_state1,
-        tracklist: None,
-        tracklist_state: list_state2,
-        focused: tui::focused_block::Playlist,
-        state: tui::menu_state::Playlist,
+    // TODO add error handeling
+    let mut home_dir = env::home_dir().unwrap();
+    home_dir.push(".local/share/ytui");
+    let mut playlists = playlist_helper{
+        playlists_path: home_dir
     };
-    tui::start_tui(&mut app);
+    playlists.list_playlist();
+
 }
--- src/playlists.rs
+++ src/playlists.rs
@@ -1,7 +1,26 @@
+use std::path::PathBuf;
+use std::fs::{read_to_string, read_dir};
+use serde_json::json;
+
 pub struct song{
     pub id:String,
     pub name: String,
     pub author: String
+}
+
+pub struct playlist_helper{
+    pub playlists_path: PathBuf
+}
+
+impl playlist_helper{
+    pub fn list_playlist(self){
+        let files = read_dir(self.playlists_path).unwrap();
+
+        for file in files {
+            println!("Name: {}", file.unwrap().path().display());
+        }
+    }
+
 }
 
 // This is debug func, as of right now it gives back test func
